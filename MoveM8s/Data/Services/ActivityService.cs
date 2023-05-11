832d6b115c20e6d812012ca27c8553ae6c2b3a20
Johanna Larsson <Johanna.Larsson@netonnet.com>
2023-05-11 20:08:04 +0200

    refactor to component

--- MoveM8s/Data/Services/ActivityService.cs
+++ MoveM8s/Data/Services/ActivityService.cs
@@ -4,13 +4,13 @@ namespace MoveM8s.Data.Services;
 
 public class ActivityService
 {
-    public async Task<IEnumerable<Activity>> GetActivities()
+    public async Task<IEnumerable<ActivityFeature>> GetActivities()
     {
-        return new List<Activity>
+        return new List<ActivityFeature>
         {
-            new Activity { Title = "Simma", Description = "Ta på dig badkläderna och ta några simtag i fin natur", Location = "Almenäs" },
-            new Activity { Title = "Hundlek", Description = "Ta med dig din fyrbenta bästis och njut av det fina vädret ihop", Location = "Kransmossen" },
-            new Activity { Title = "Promenera", Description = "Njut av vackra omgivningar längs med viskan på grusade gångar där du även kan ta med dig barnvagnen", Location = "Stadsparken" }
+            new ActivityFeature { Name = "Simma", Description = "Ta på dig badkläderna och ta några simtag i fin natur", VisitUrl = "Almenäs" },
+            new ActivityFeature { Name = "Hundlek", Description = "Ta med dig din fyrbenta bästis och njut av det fina vädret ihop", VisitUrl = "Kransmossen" },
+            new ActivityFeature { Name = "Promenera", Description = "Njut av vackra omgivningar längs med viskan på grusade gångar där du även kan ta med dig barnvagnen", VisitUrl = "Stadsparken" }
         };
     }
 }
