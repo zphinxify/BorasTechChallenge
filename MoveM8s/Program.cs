757a97f4ddcb6d2bf9c4c87e8cb50aa14a502d5c
Johanna Larsson <Johanna.Larsson@netonnet.com>
2023-05-11 17:38:19 +0200

    Some basic stuff and mocked data

--- MoveM8s/Program.cs
+++ MoveM8s/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MoveM8s.Data;
+using MoveM8s.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ActivityService>();
 
 var app = builder.Build();
 
