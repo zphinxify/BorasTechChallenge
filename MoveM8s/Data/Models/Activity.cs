484871fab543b07ecc00bd5d4223e3a44a14ae32
Johanna Larsson <Johanna.Larsson@netonnet.com>
2023-05-11 19:47:49 +0200

    Update

--- MoveM8s/Data/Models/Activity.cs
+++ MoveM8s/Data/Models/Activity.cs
@@ -2,7 +2,7 @@
 
 public class Activity
 {
-    public string Title { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
-    public string Location { get; set; }
+    public string VisitUrl { get; set; }
 }
